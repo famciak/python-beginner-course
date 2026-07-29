afb3f064eb458b7526c8bcc2e814c071535d61c4
famciak <famciak@gmail.com>
2026-07-30 01:41:06 +0330

    Update main.py

--- 02-conditional-statements/main.py
+++ 02-conditional-statements/main.py
@@ -1,1 +1,51 @@
+print(" ")
+
+
+name =input("please enter your name : ")
+
+print(" ")
+
+age = int(input("please enter your age : "))
+
+print(" ")
+
+
+if age >= 18:
+
+    midterm = int(input("please input your midterm : "))
+
+    print(" ")
+    
+    final_term = int(input("please input your final term "))
+    
+    print(" ")
+
+    sum = (midterm + final_term) //2
+    
+    print(f"your avrage point is : {sum}")
+
+    
+    print(" ")
+
+
+    if sum <= 50:
+    
+        
+        print("you were failed in exam > you can try it later " ) 
+        
+        print(" ")
+    
+    elif sum >=50 and sum < 70:
+    
+        print(" you were passed the exam ")
+    
+    else:
+
+        sum >= 70 and sum >= 100
+
+        print("exllent")
+
+else:
+
+   print("you are a teenager try again later ")
 
