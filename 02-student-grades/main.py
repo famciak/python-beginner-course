4765d7220849b45e8c5344b53c6536289e077560
famciak <famciak@gmail.com>
2026-07-30 02:13:11 +0330

    Update main.py

--- 02-student-grades/main.py
+++ 02-student-grades/main.py
@@ -1,1 +1,6 @@
-
+midterm = 77
+print(f"your midterm is {midterm}")
+final_term = 23
+print(f"your final term is {final_term}")
+sum = midterm + final_term 
+print(f"your grade is  {midterm} + {final_term} = {sum}")
