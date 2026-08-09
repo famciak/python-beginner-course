94b7e414d569e002413fb9bab54fdbf61637df49
famciak <famciak@gmail.com>
2026-08-09 05:37:57 +0330

    Update main.py

--- 05-def-structure/main.py
+++ 05-def-structure/main.py
@@ -10,5 +10,5 @@ num1 = max(min(float(input("please enter your {num1} : ")),20),0)
 num2 = max(min(float(input("please enter your {num2} : ")),20),0)
 num3 = max(min(float(input("please enter your {num3} : ")),20),0)
 print(num1,num2,num3)
-print(f"minimal : ",minimal(num1,num2,num3), "maximal : ", maximal(num1,num2,num3) ,"avrage : ",avrege(num1,num2,num3),"total : ",total(num1,num2,num3))
+print("minimal : ",minimal(num1,num2,num3),"|", "maximal : ", maximal(num1,num2,num3),"|" ,"avrage : ",avrege(num1,num2,num3),"|","total : ",total(num1,num2,num3))
 
